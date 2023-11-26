f2fc52c2280ee93c567656b976f9e487d100dc6d
Victor Momodu <victoramomodu@gmail.com>
2023-11-26 22:31:40 +0100

    Changed format of pages

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -1,8 +1,8 @@
 import streamlit as st
-from pages import home, data_preprocessing
+from pages import data_preprocessing
 
 # Create a dictionary to map page names to functions
-pages = {"Home": home.app, "Data Input": data_preprocessing.app}
+pages = {"Data Input": data_preprocessing.app}
 
 # Create a sidebar to select pages
 selected_page = st.sidebar.selectbox("Select Page", list(pages.keys()))
