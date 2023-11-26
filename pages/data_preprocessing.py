6fb979be540d3fa96155d20acd0644005be65391
Victor Momodu <victoramomodu@gmail.com>
2023-11-26 22:18:59 +0100

    Added streamlit files

---
+++ pages/data_preprocessing.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def app():
+    st.title("Data Preprocessing Page")
+    # Your data input page content here
---
+++ pages/home.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def app():
+    st.title("Home Page")
+    # Your home page content here
