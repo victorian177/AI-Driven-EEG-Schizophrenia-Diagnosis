538cb1cb7091cc4c1f4578641e9b6e47217bbd18
Victor Momodu <victoramomodu@gmail.com>
2023-11-26 22:54:52 +0100

    Update 🏠_Home.py

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -2,8 +2,4 @@ import streamlit as st
 
 st.set_page_config(page_title="Home")
 
-st.markdown(
-    """
-    Streamlit is an open-source app framework built specifically for
-"""
-)
+st.write("Streamlit is an open-source app framework built specifically for")
