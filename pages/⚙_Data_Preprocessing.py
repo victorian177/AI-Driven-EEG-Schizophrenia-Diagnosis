0cf59cce0e314b63e0b79402b941da4dd53bd561
Victor Momodu <victoramomodu@gmail.com>
2023-11-26 22:27:45 +0100

    Changed Streamlit pages

--- pages/⚙_Data_Preprocessing.py
+++ pages/⚙_Data_Preprocessing.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
+
 def app():
     st.title("Data Preprocessing Page")
     # Your data input page content here
--- 🏠_Home.py
+++ 🏠_Home.py
@@ -2,10 +2,7 @@ import streamlit as st
 from pages import home, data_preprocessing
 
 # Create a dictionary to map page names to functions
-pages = {
-    "Home": home.app,
-    "Data Input": data_preprocessing.app
-}
+pages = {"Home": home.app, "Data Input": data_preprocessing.app}
 
 # Create a sidebar to select pages
 selected_page = st.sidebar.selectbox("Select Page", list(pages.keys()))
