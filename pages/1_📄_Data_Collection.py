decc3eada1bf2038d4ba85abbf83bd007fb5a7c8
Victor Momodu <victoramomodu@gmail.com>
2023-12-04 00:39:46 +0100

    Update 1_📄_Data_Collection.py

--- pages/1_📄_Data_Collection.py
+++ pages/1_📄_Data_Collection.py
@@ -29,8 +29,8 @@ st.write(
 #### Demographic Information:
 
 - **Age:**
-  - Patients: {patients["age"].mean():2f} ± {patients["age"].std():2f}
-  - Controls: {controls["age"].mean():2f} ± {controls["age"].std():2f}
+  - Patients: {patients["age"].mean():.2f} ± {patients["age"].std():.2f}
+  - Controls: {controls["age"].mean():.2f} ± {controls["age"].std():.2f}
 
 - **Gender Distribution:**
   - Patients: [Number of Males / Number of Females]
