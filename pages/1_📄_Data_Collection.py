faabe144892f0028345679413b2b6589ae68a729
Victor Momodu <victoramomodu@gmail.com>
2023-12-03 20:47:02 +0100

    Update 1_📄_Data_Collection.py

--- pages/1_📄_Data_Collection.py
+++ pages/1_📄_Data_Collection.py
@@ -1,4 +1,6 @@
+import pandas as pd
 import streamlit as st
+
 
 st.write(
     """
@@ -7,18 +9,22 @@ st.write(
 
         ## Scope of the Report
         1. **Participants:** A detailed overview of the individuals involved in the study, including patients diagnosed with schizophrenia and the control group.
-
         2. **Data Collection Methodology:** An in-depth explanation of the procedures, tools, and protocols utilized for gathering EEG data.
-
         3. **Data Distributions:** Presentation and analysis of various distributions within the collected data, highlighting patterns and differences between patient and control groups.
 
         The insights derived from this comprehensive data collection process are pivotal for the subsequent stages of our project, contributing to the development of a machine learning model for the early detection of schizophrenia.
         """
 )
 
+participants = pd.read_csv("participant_info.csv")
+
+
 st.write(
-    """
+    f"""
 ### Participants
+
+### Dataset
+{st.dataframe(participants)}
 
 #### Demographic Information:
 
@@ -27,48 +33,24 @@ st.write(
   - Controls: Mean age ± SD
 
 - **Gender Distribution:**
-  - Patients: [Number of Males / Number of Females / Other]
-  - Controls: [Number of Males / Number of Females / Other]
+  - Patients: [Number of Males / Number of Females]
+  - Controls: [Number of Males / Number of Females]
 
 - **Ethnicity (if applicable):**
   - Provide a breakdown of the ethnic composition of the participants.
-
-#### Clinical Characteristics (for patients):
-
-- **Duration of Illness:**
-  - Average duration of schizophrenia ± SD (if applicable)
-
-- **Clinical Assessments:**
-  - Mention any relevant clinical assessments conducted for patients.
 
 #### Inclusion and Exclusion Criteria:
 
 - Clearly defined inclusion criteria for both patient and control groups.
 - Clearly defined exclusion criteria for both patient and control groups.
 
-#### Participant Recruitment:
-
-- Describe the methods used for participant recruitment, such as clinical settings, advertisements, etc.
-
 #### Sample Size:
 
 - Total number of participants: [Number of Patients] (Schizophrenia) + [Number of Controls]
 
-#### Ethical Considerations:
-
-- Emphasize that the study adhered to ethical guidelines, including participant consent and privacy protection.
-
 #### Participant IDs (Anonymization):
 
 - Briefly explain the use of participant IDs to ensure anonymity.
-
-#### Participant Compensation (if applicable):
-
-- State if participants received compensation and provide details.
-
-#### Voluntary Participation:
-
-- Reiterate that participation was voluntary, and participants had the right to withdraw without consequences.
 
 #### Description of Control Group (if applicable):
 
